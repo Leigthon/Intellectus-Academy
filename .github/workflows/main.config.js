5f784d00f8c157082082b5b9abe1a2c8ca0926d4
Leigthon Messina <137066770+Leigthon@users.noreply.github.com>
2026-02-01 00:20:56 +0200

    Set base path for Vite configuration

--- .github/workflows/main.config.js
+++ .github/workflows/main.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
+  base: "Leigthon/Intellectus-Academy",
   plugins: [react()],
   resolve: {
     alias: {
