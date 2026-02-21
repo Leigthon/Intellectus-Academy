7b484dcf809bff1bec644fea865d329b6e3cf415
Leigthon Messina <137066770+Leigthon@users.noreply.github.com>
2026-02-21 23:20:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
-  base: "/Intellectus-Academy/", // Must match repo name exactly with slashes
+  base: "/", // Must match repo name exactly with slashes
   plugins: [react()],
   resolve: {
     alias: {
